fef14e8bd98e7e60d872dbce0dba7480b6376e56
jannunzi <jannunzi@gmail.com>
2014-05-22 15:57:04 -0400

    Wrap trip id in ObjectId

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.put("/api/user/:username/trip/:tripId/fish/:id", function(req, res){
 	console.log(req.params.id);
 	console.log(req.body);
 	delete req.body._id;
-	delete req.body.trip_id;
+	req.body.trip_id = mongojs.ObjectId(req.params.tripId);
 	db.fish.update({_id: mongojs.ObjectId(req.params.id)}, req.body, function(err, newFish){
 		console.log("err:");
 		console.log(err);
