fe4798c4dcec1b96f4c3ccc0ce64bb0366fc8bf2
jannunzi <jannunzi@gmail.com>
2014-05-22 10:49:45 -0400

    Create Fish

--- public/js/fish.js
+++ public/js/fish.js
@@ -16,41 +16,18 @@ f360.controller("NewFishController", function($scope, $routeParams, $http, $loca
 	$scope.tripId = $routeParams.tripId;
 	$scope.create = function()
 	{
+		console.log("NewFishController.create()");
 		$scope.newTrip.username = $scope.username;
 		$scope.newTrip.trip_id = $scope.tripId;
-		$http.post("api/user/"+$scope.username+"/trip/"+$scope.tripId+"/fish", $scope.newTrip)
+		console.log($routeParams.username);
+		console.log($routeParams.tripId);
+		var url = "api/user/"+$scope.username+"/trip/"+$scope.tripId+"/fish", $scope.newTrip;
+		console.log(url);
+		$http.post(url)
 		.success(function(trips)
 		{
+			console.log("NewFishController.create() post callback");
 			$location.path( username+"/trip/"+$scope.tripId+"/fish/list" );
 		});
-	}
-});
-
-f360.controller("EditTripController", function($scope, $routeParams, $http, $location)
-{
-	var username = $routeParams.username;
-	var tripid = $routeParams.tripid;
-	
-	$http.get("api/"+username+"/trip/"+tripid)
-		.success(function(trip)
-		{
-			$scope.editTrip = trip;
-		});
-	
-	$scope.update = function()
-	{
-		$scope.editTrip.username = username;
-		$http.put("api/"+username+"/trip/"+tripid, $scope.editTrip)
-			.success(function(trip){
-				$location.path( username+"/trip/list" );
-			});
-	}
-	
-	$scope.delete = function()
-	{
-		$http.delete("api/"+username+"/trip/"+tripid)
-			.success(function(trip){
-				$location.path( username+"/trip/list" );
-			});
 	}
 });
