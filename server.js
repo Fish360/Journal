be03eea083cc06dcca4ae260220cdd8807420797
jannunzi <jannunzi@gmail.com>
2014-05-22 13:59:48 -0400

    Update Fish

--- server.js
+++ server.js
@@ -142,6 +142,7 @@ app.delete("/api/user/:username/trip/:tripid/fish/:fishid", function(req, res){
 app.put("/api/user/:username/trip/:tripId/fish/:id", function(req, res){
 	console.log(req.params.id);
 	console.log(req.body);
+	delete req.body._id;
 	db.fish.update({_id: mongojs.ObjectId(req.params.id)}, req.body, function(err, newFish){
 		console.log("err:");
 		console.log(err);
