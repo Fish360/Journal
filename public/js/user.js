c07d26ca769414a6b56a3d4c5cb130431998b84c
jannunzi <jannunzi@gmail.com>
2014-06-08 09:25:10 -0400

    Minor login changes

--- public/js/user.js
+++ public/js/user.js
@@ -2,7 +2,7 @@
 f360.controller("LoginController", function($scope, $routeParams, $http, $location)
 {
 	$scope.message = "";
-	$scope.username = "user1";
+	$scope.username = "";
 	console.log("LoginController");
 	$scope.login = function() {
 		$scope.message = "";
