7ee767ad54663c8917a6e2fefe3b6d2e6ab9dc7d
jannunzi <jannunzi@gmail.com>
2014-07-25 12:31:18 -0400

    Password

--- server.js
+++ server.js
@@ -152,8 +152,8 @@ app.put("/api/user/:username", function(req, res)
 {
 	console.log(req.body);
 	delete req.body._id;
-//	if(req.body.password != req.body.password2 || req.body.password == "" || typeof req.body.password == "undefined" )
-	delete req.body.password;
+	if(req.body.password != req.body.password2 || req.body.password == "" || typeof req.body.password == "undefined" )
+		delete req.body.password;
 	delete req.body.password2;
 	db.user.findAndModify({
 		query: {username: req.params.username},
