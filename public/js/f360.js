823ccc00aa50415e5995edf55234e646779b1c82
jannunzi <jannunzi@gmail.com>
2014-09-22 22:07:59 -0400

    :)

--- public/js/f360.js
+++ public/js/f360.js
@@ -1,5 +1,27 @@
 var f360 = angular.module("f360", ["ngRoute"]);
 
+f360.directive('siteHeader', function () {
+    return {
+        restrict: 'E',
+        template: '<button class="btn">{{back}}</button><button class="btn">{{forward}}</button>',
+        scope: {
+            back: '@back',
+            forward: '@forward',
+            icons: '@icons'
+        },
+        link: function(scope, element, attrs) {
+            $(element[0]).on('click', function() {
+                history.back();
+                scope.$apply();
+            });
+            $(element[1]).on('click', function() {
+                history.forward();
+                scope.$apply();
+            });
+        }
+    };
+});
+
 f360.config(["$routeProvider", function($routeProvider, $http)
 {
 	$routeProvider
