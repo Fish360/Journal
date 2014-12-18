42aa903ed1470b28942ebc63a475d4da0b19f408
jannunzi <jannunzi@gmail.com>
2014-12-18 12:00:10 -0500

    Trying to fix

--- public/js/fish.js
+++ public/js/fish.js
@@ -90,6 +90,11 @@ f360.controller("NewFishController", function ($scope, $routeParams, $http, $loc
 
 f360.controller("EditFishController", function ($scope, $routeParams, $http, $location, SpotService, GearService, PresentationsService)
 {
+
+	var numeric = jQuery('.f360-number');
+	console.log(numeric);
+	numeric.autoNumeric('init',{aDec: '.', aPad: true});
+	
 	$scope.speciess = species;
 
 	$scope.username = $routeParams.username;
