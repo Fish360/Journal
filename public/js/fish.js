3718eebdffe3fea4b7d55f446cc880b5bfd41310
hsamogh <hsamogh@ccs.neu.edu>
2016-04-09 13:31:12 -0400

    'issue332'

--- public/js/fish.js
+++ public/js/fish.js
@@ -73,7 +73,7 @@ f360.controller("NewFishController", function ($scope, $routeParams, $http, $loc
 	$scope.speciess = species;
 	$scope.username = $routeParams.username;
 	$scope.tripId = $routeParams.tripId;
-	
+
 	loadSpecies();
 
 	function loadSpecies() {
@@ -86,10 +86,26 @@ f360.controller("NewFishController", function ($scope, $routeParams, $http, $loc
 	TripService.findOne($scope.username, $scope.tripId, function(trip){
 		if(trip.start !== undefined) {
 			var dateParts = (trip.start).split("-");
-			var date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2])
+			var date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
 			var tripDate = moment(date).format("YYYY-MM-DD");
+			console.log("trip start and end");
+			console.log(trip.start);
+			console.log(trip.end);
+			$scope.newFish.minDate=trip.start.toString();
+			console.log($scope.newFish.minDate);
 
-			$scope.newFish.caught = tripDate;
+			$scope.newFish.maxDate= trip.end.toString();
+			console.log($scope.newFish.maxDate);
+
+			if(tripDate>=trip.start && tripDate<=trip.end)
+			{
+				$scope.newFish.caught = tripDate;
+			}
+			else{
+				alert("Fish Caught date must be within Trip Date range");
+			}
+
+
 		}
 	});
 
@@ -109,9 +125,15 @@ f360.controller("NewFishController", function ($scope, $routeParams, $http, $loc
 	
 	$scope.create = function()
 	{
+		//console.log("We are in create");
 		if(!validateSpecieSelection()){
 			return;
 		}
+		if(!validateFishDates()){
+			return;
+		}
+
+
 
 		var url = "api/user/"+$scope.username+"/trip/"+$scope.tripId+"/fish",
 			scientificName = ($scope.newFish.species.originalObject === undefined) ? $scope.newFish.species : $scope.newFish.species.originalObject["ScientificName"];
@@ -157,6 +179,23 @@ f360.controller("NewFishController", function ($scope, $routeParams, $http, $loc
 //	SpotService.findAll($scope.username, function (response) {
 //	    $scope.newFish.spots = response;
 //	});
+
+	function validateFishDates(){
+		var caughtDate=$scope.newFish.caught;
+		console.log(caughtDate);
+		var trip = TripService.findOne($scope.username,$scope.tripId,function(trip){
+			if(trip.start<=caughtDate && trip.end >= caughtDate){
+				return true;
+			}
+			else{
+				alert("Fish Caught date must be within trip range");
+				//$location.url("/"+$scope.username+"/trip/"+$scope.tripId+"/fish/new");
+				return false;
+			}
+		});
+		//console.log(trip);
+		//return true;
+	}
 
 	function validateSpecieSelection() {
 		var isValidSpecies = ($scope.newFish.species === undefined)? false : true;
@@ -217,8 +256,13 @@ f360.controller("EditFishController", function ($scope, $routeParams, $http, $lo
 				var dateParts = (trip.start).split("-");
 				var date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2])
 				var tripDate = moment(date).format("YYYY-MM-DD");
-
-				$scope.editFish.caught = tripDate;
+                if(tripDate>=trip.start && tripDate<=trip.end)
+				{
+					$scope.editFish.caught = tripDate;
+				}
+				else{
+					alert("Fish Caught date must be within Trip Date range");
+				}
 			}
 		});
 	}
@@ -287,6 +331,7 @@ f360.controller("EditFishController", function ($scope, $routeParams, $http, $lo
 			return false;
 		}
 	}
+
 
 	function validateSpecieSelection() {
 		var isValidSpecies = ($scope.editFish.species === undefined)? false : true;
