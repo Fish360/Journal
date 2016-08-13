9d94d03c466d50e6a6baf3911323b1bed0d57f71
Jose Annunziato <jannunzi@gmail.com>
2016-08-13 19:51:29 -0400

    update weather api key

--- public/services/weatheronline.service.client.js
+++ public/services/weatheronline.service.client.js
@@ -4,8 +4,9 @@
         .factory("WorldWeatherOnlineService", WorldWeatherOnlineService);
 
     var api = "http://api.worldweatheronline.com/premium/v1/past-marine.ashx?key=b7dd5188ce31412fb3f220036163005&q=70,45&format=json&includelocation=yes&tide=yes&tp=24";
-    var key = "c5256aff2a4f44c4917233610161306";
-    var baseUrl = "http://api.worldweatheronline.com/premium/v1/past-marine.ashx?callback=JSON_CALLBACK&key=b7dd5188ce31412fb3f220036163005&format=json&includelocation=yes&tide=yes&tp=24&q=LATLONG&date=DATE";
+    // var key = "c5256aff2a4f44c4917233610161306";
+    var key = "14c2d659cd53449ea01234024161308";
+    var baseUrl = "http://api.worldweatheronline.com/premium/v1/past-marine.ashx?callback=JSON_CALLBACK&key=API_KEY&format=json&includelocation=yes&tide=yes&tp=24&q=LATLONG&date=DATE";
 
     function WorldWeatherOnlineService($http) {
         var api = {
@@ -25,6 +26,7 @@
             var latlong = lat+","+long;
             var url = baseUrl.replace("LATLONG", latlong);
             url = url.replace("DATE", date);
+            url = url.replace("API_KEY", key);
             return $http.jsonp(url);
         }
     }
