a7995dea805f848d723f9f354e5c3b5ad31fdd03
Jose Annunziato <jannunzi@gmail.com>
2016-08-18 23:32:24 -0400

    added findSunMoonPhase2 so no need to pass fish or spot obejcts

--- public/services/sunmoon.service.client.js
+++ public/services/sunmoon.service.client.js
@@ -2,15 +2,23 @@
     f360.factory("SunMoonService", SunMoonService);
     function SunMoonService ($http) {
         var model = {
-            findSunMoonPhase: findSunMoonPhase
+            findSunMoonPhase: findSunMoonPhase,
+            findSunMoonPhase2: findSunMoonPhase2
         };
         return model;
 
         function findSunMoonPhase (fish,spot,callback) {
             $http.get("/api/sunmoonphase/"+fish.caught+"/latitude/"+spot.latitude+"/longitude/"+spot.longitude)
-            .success(function(response){
-                callback(response);
-            });
-         }
+                .success(function(response){
+                    callback(response);
+                });
+        }
+
+        function findSunMoonPhase2 (date,latitude,longitude,callback) {
+            $http.get("/api/sunmoonphase/"+date+"/latitude/"+latitude+"/longitude/"+longitude)
+                .success(function(response){
+                    callback(response);
+                });
+        }
     }
 })();
