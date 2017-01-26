0354168efcb7fb8d407192ad5f1310e0f2a94728
Jose Annunziato <jannunzi@gmail.com>
2017-01-25 23:32:45 -0500

    removed alerts

--- public/js/user.js
+++ public/js/user.js
@@ -87,18 +87,14 @@ f360.controller("ProfileController", function($scope, $routeParams, $http, $loca
 f360.controller("LoginController2", function($scope, $routeParams, $http, $location)
 {
 	$scope.login = function () {
-		alert('login function: ' + $scope.username + " " + $scope.password);
 		$scope.message = "";
 		$http.get("/api/user/"+$scope.username+"/"+$scope.password)
 			.success(function(user){
-				alert(user.username);
 				if(!user) {
-					alert('user not found');
 					$scope.message = "Username and/or password does not exist. Try again";
 				}
 				else {
 //				$location.path( $scope.username+"/trip/list" );
-					alert('going home');
 					$location.path( $scope.username+"/home" );
 					// localStorage.setItem("user", JSON.stringify(user[0]));
 					// var encryptedPassword = $scope.password;//CryptoJS.AES.encrypt($scope.password, "password");
@@ -113,25 +109,20 @@ f360.controller("LoginController", function($scope, $routeParams, $http, $locati
 {
 	$scope.message = "";
 	$scope.username = "";
-	alert("LoginController 234");
 
 	$scope.test = function () {
 		alert('test()');
 	}
 
 	$scope.login = function() {
-		alert('in login() function');
 		$scope.message = "";
 		$http.get("/api/user/"+$scope.username+"/"+$scope.password)
 		.success(function(user){
-			alert(user.username);
 			if(!user) {
-				alert('user not found');
 				$scope.message = "Username and/or password does not exist. Try again";
 			}
 			else {
 //				$location.path( $scope.username+"/trip/list" );
-				alert('going home');
 				$location.path( $scope.username+"/home" );
 				localStorage.setItem("user", JSON.stringify(user[0]));
 				var encryptedPassword = $scope.password;//CryptoJS.AES.encrypt($scope.password, "password");
