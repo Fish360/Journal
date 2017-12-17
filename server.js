56cf12e125a9abaf726d02a07f980e865ade1fb6
Samarth Raghuram Shetty <samarthshetty1990@ccs.neu.edu>
2017-12-17 17:04:47 -0500

    Removed uploads directory requirement

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var gateway = braintree.connect({
 app.configure(function () {
     app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
     app.use(express.logger('dev')); 						// log every request to the console
-    app.use(express.bodyParser({ uploadDir: './public/uploads' })); 							// pull information from html in POST
+    app.use(express.bodyParser()); 							// pull information from html in POST
     app.use(express.json()); 							// pull information from html in POST
     app.use(express.urlencoded());
     app.use(express.methodOverride()); 						// simulate DELETE and PUT
